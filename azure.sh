6f66a1849245692b92e51a5c46c30356ae9b270c
Vinay244 <80942635+Vinay244@users.noreply.github.com>
2024-02-27 12:58:37 +0530

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# azcopy copy /home/vinay/C_POC/hello.c "https://vinaystorage108.blob.core.windows.net/newone/hello.c?sp=racwd&st=2024-02-26T18:46:16Z&se=2024-02-29T02:46:16Z&spr=https&sv=2022-11-02&sr=c&sig=6VAD77nHGhNo3UfhjaipPSDTsvm0JWz4M13rLrbe0%2Bk%3D"
+azcopy copy hello.c "https://vinaystorage108.blob.core.windows.net/newone/hello.c?sp=racwd&st=2024-02-26T18:46:16Z&se=2024-02-29T02:46:16Z&spr=https&sv=2022-11-02&sr=c&sig=6VAD77nHGhNo3UfhjaipPSDTsvm0JWz4M13rLrbe0%2Bk%3D"
 # Pull the Azure CLI Docker image
-docker pull mcr.microsoft.com/azure-cli
+#docker pull mcr.microsoft.com/azure-cli
 # Run azcopy command within the Docker container
-docker run --rm -v /home/vinay:/data mcr.microsoft.com/azure-cli azcopy copy /data/vinay.c "https://vinaystorage108.blob.core.windows.net/newone/vinay.c?sp=racwd&st=2024-02-26T18:46:16Z&se=2024-02-29T02:46:16Z&spr=https&sv=2022-11-02&sr=c&sig=6VAD77nHGhNo3UfhjaipPSDTsvm0JWz4M13rLrbe0%2Bk%3D"
+#docker run --rm -v /home/vinay:/data mcr.microsoft.com/azure-cli azcopy copy /data/vinay.c "https://vinaystorage108.blob.core.windows.net/newone/vinay.c?sp=racwd&st=2024-02-26T18:46:16Z&se=2024-02-29T02:46:16Z&spr=https&sv=2022-11-02&sr=c&sig=6VAD77nHGhNo3UfhjaipPSDTsvm0JWz4M13rLrbe0%2Bk%3D"
