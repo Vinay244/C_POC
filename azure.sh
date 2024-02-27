14db8f000548aa77853f8268f0019a1bbbf394b0
Vinay244 <80942635+Vinay244@users.noreply.github.com>
2024-02-27 12:32:49 +0530

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 azcopy copy /home/vinay/hello.c "https://vinaystorage108.blob.core.windows.net/newone/hello.c?sp=racwd&st=2024-02-26T18:46:16Z&se=2024-02-29T02:46:16Z&spr=https&sv=2022-11-02&sr=c&sig=6VAD77nHGhNo3UfhjaipPSDTsvm0JWz4M13rLrbe0%2Bk%3D"
